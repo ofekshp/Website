4cda8c2febf623c4f1de0786bc083dcaaa41eeb8
Ofek Shpirer <ofekshp2821@gmail.com>
2025-07-08 11:57:53 +0300

    add + fix

--- my-portfolio/src/components/sections/Hero.tsx
+++ my-portfolio/src/components/sections/Hero.tsx
@@ -1,5 +1,8 @@
 const Hero = () => (
-  <section className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white text-center p-4 sm:p-8">
+  <section
+    id="hero"
+    className="min-h-[75vh] flex flex-col justify-center items-center bg-gray-900 text-white text-center p-4 sm:p-8"
+  >
     <h1 className="text-5xl sm:text-6xl font-bold mb-4">Ofek Shpirer</h1>
     <p className="text-base sm:text-xl mb-4">Full Stack Developer</p>
     <p className="text-sm sm:text-lg max-w-md sm:max-w-xl mb-6">
